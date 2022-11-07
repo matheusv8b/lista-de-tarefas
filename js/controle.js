deb7653da6c1599982346ef8dfc3813a2f664289
Matheus de Jesus <matheusv0b@hotmail.com>
2022-11-07 16:21:28 -0300

    Parte Final
    Parte Final

--- js/controle.js
+++ js/controle.js
@@ -1,3 +1,4 @@
+let contador = 0;
 let input = document.getElementById('inputTarefa');
 let btnAdd = document.getElementById('btn-add');
 let main = document.getElementById('areaLista');
@@ -8,15 +9,18 @@ function addTarefa(){
 
     //SE NÃO FOR VAZIO, NEM NULO, NEM INDEFINIDO
     if((valorInput !== "") && (valorInput !== null) && (valorInput !== undefined)) {
-        let novoItem = `<div class="item">
-        <div class="item-icone">
-            <i class="mdi mdi-circle-outline"></i>
+
+        ++contador;
+
+        let novoItem = `<div id="${contador}" class="item">
+        <div onclick="marcarTarefa(${contador})" class="item-icone">
+            <i id="icone_${contador}" class="mdi mdi-circle-outline"></i>
         </div>
-        <div class="item-nome">
+        <div onclick="marcarTarefa(${contador})" class="item-nome">
             ${valorInput}
         </div>
         <div class="item-botao">
-            <button class="delete"><i class="mdi mdi-delete">Deletar</i></button>
+            <button onclick="deletar(${contador})" class="delete"><i class="mdi mdi-delete">Deletar</i></button>
         </div>
     </div>`;
 
@@ -26,6 +30,33 @@ function addTarefa(){
     //Zerar os  Campos
     input.value = "";
     input.focus();
+    }
+}
+
+function deletar(id) {
+    var tarefa = document.getElementById(id);
+    tarefa.remove();
+}
+
+function marcarTarefa(id){
+    var item = document.getElementById(id);
+    var classe = item.getAttribute('class');
+    
+    if (classe == "item") {
+        item.classList.add('clicado')
+
+        var icone = document.getElementById('icone_' +id);
+        icone.classList.remove('mdi-circle-outline');
+        icone.classList.add('mdi-check-circle');
+
+        item.parentNode.appendChild(item);
+
+    } else{
+        item.classList.remove('clicado');
+
+        var icone = document.getElementById('icone_' +id);
+        icone.classList.remove('mdi-check-circle');
+        icone.classList.add('mdi-circle-outline');
     }
 }
 
